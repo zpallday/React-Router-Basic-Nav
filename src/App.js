93fbf13677bbbb0a70780ae0eada3d8cbab49a78
zachary-peasley <zpman95@hotmail.com>
2019-08-19 15:09:39 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import {router} from 'react-router-dom;'
+import {Router} from 'react-router-dom';
 
 
 const App = () => (
   <div>
     <Navigation />
+    <Router path="/" exact component={Home} />
+    <Router path='/about' component={About} />
+    <Router path='/contact' component={Contact}/>
   </div>
 );
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import Home from './Home';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from '../App';
 
 
 export { Home, About, Contact, Navigation };
 
 
-ReactDOM.render(
-<Router>
-    <App />
-</Router>,
-document.getElementById('root')
-);
+
+ReactDOM.render( 
+<Router> 
+    <App/>
+    </Router>, 
+    document.getElementById('root'));
