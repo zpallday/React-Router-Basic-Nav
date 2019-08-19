0215ec26c0270df56b43ea2dba60ed51689ee98d
zachary-peasley <zpman95@hotmail.com>
2019-08-19 14:42:15 -0500

    index Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {router} from 'react-router-dom;'
+
 
 const App = () => (
   <div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,17 @@
 import Home from './Home';
+import ReactDOM from "react-dom";
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
+import {BrowserRouter as Router} from "react-router-dom";
+
 
 export { Home, About, Contact, Navigation };
+
+
+ReactDOM.render(
+<Router>
+    <App />
+</Router>,
+document.getElementById('root')
+);
