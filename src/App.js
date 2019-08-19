e7a3a3c917bdfa080ab7c792022dd082071f4a4f
zachary-peasley <zpman95@hotmail.com>
2019-08-19 15:36:53 -0500

    fixed errors, MVP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import {Router} from 'react-router-dom';
+import {Route} from "react-router-dom";
 
+const App = () => {
 
-const App = () => (
-  <div>
-    <Navigation />
-    <Router path="/" exact component={Home} />
-    <Router path='/about' component={About} />
-    <Router path='/contact' component={Contact}/>
-  </div>
+return(
+ <div>
+
+  <Route exact path="/" component={Home}/>
+  <Route exact path="/about" component={About}/>
+  <Route exact path="/contact" component={Contact}/>
+
+  <Navigation />
+
+</div>
+
 );
 
+  
+
+ }
+
 export default App;
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,6 @@
-import React from 'react';
 import Home from './Home';
-import ReactDOM from 'react-dom';
 import About from './About';
 import Contact from './Contact';
 import Navigation from './Navigation';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from '../App';
-
 
 export { Home, About, Contact, Navigation };
-
-
-
-ReactDOM.render( 
-<Router> 
-    <App/>
-    </Router>, 
-    document.getElementById('root'));
